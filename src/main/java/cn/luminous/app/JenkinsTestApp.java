d62c229084d6b449d3f04f1b6b898a69a9d2a447
wangdewei <Wangdewei121@gmail.com>
2025-04-16 15:37:46 +0800

    测试jenkins 项目管理

---
+++ src/main/java/cn/luminous/app/JenkinsTestApp.java
@@ -0,0 +1,19 @@
+package cn.luminous.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: jenkins-test
+ * @description:
+ * @author: wang
+ * @create: 2025-04-16 15:25
+ **/
+@SpringBootApplication
+public class JenkinsTestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApp.class,args);
+    }
+
+}
